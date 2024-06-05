2e21f23899c8b4474a6cc4f84632b7c0449da637
Wésley Guimarães <wes@guima.dev>
2024-06-05 13:52:30 -0300

    feat: isometric

--- apps/web-client/src/components/Canvas.tsx
+++ apps/web-client/src/components/Canvas.tsx
@@ -1,30 +1,42 @@
 import { Application } from '@pixi/app'
+import { Container } from '@pixi/display'
 import { Graphics } from '@pixi/graphics'
 import type { Cell } from '@pkgs/db'
-import { onMount, type Ref } from 'solid-js'
+import { onMount } from 'solid-js'
 
 export const app = new Application()
 app.start()
 
 type AppProps = {
-  map: Cell[][]
+  map: Cell[]
 }
 export const App = ({ map = [] }: AppProps) => {
-  let canvas: Ref<HTMLCanvasElement>
   onMount(async () => {
     const graphics = new Graphics()
     graphics.beginFill(0x004200)
     graphics.lineStyle(1, 0xffffff)
 
+    const scalingContainer = new Container()
+    scalingContainer.scale.y = 0.3 // adjust scale by Y - that will change "perspective" a bit
+    scalingContainer.position.set((app.screen.width * 3) / 8, app.screen.height / 2)
+    app.stage.addChild(scalingContainer)
+
+    const isoScalingContainer = new Container()
+    isoScalingContainer.scale.y = 0.5 // isometry can be achieved by setting scaleY 0.5 or tan(30 degrees)
+    isoScalingContainer.position.set((app.screen.width * 6) / 8, app.screen.height / 2)
+    app.stage.addChild(isoScalingContainer)
+
+    const isometryPlane = new Graphics()
+    isometryPlane.rotation = Math.PI / 4
+    isoScalingContainer.addChild(isometryPlane)
+
     const cellSize = 50
 
-    for (const row of map) {
-      for (const cell of row) {
-        graphics.drawRect(cell.x * cellSize, cell.y * cellSize, cellSize, cellSize)
-      }
+    for (const cell of map) {
+      graphics.drawRect(cell.x * cellSize, cell.y * cellSize, cellSize, cellSize)
     }
 
-    app.stage.addChild(graphics)
+    isometryPlane.addChild(graphics)
   })
 
   return app.view
