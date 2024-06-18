f3a1f56c2fce5039f98d034dd1cb745e2c25d2bf
Wésley Guimarães <wes@guima.dev>
2024-06-18 00:13:13 -0300

    fix: theme cookies

--- apps/web-client/src/actions/index.ts
+++ apps/web-client/src/actions/index.ts
@@ -1,22 +1,15 @@
-import env from '@pkgs/env'
 import { defineAction } from 'astro:actions'
 import { z } from 'zod'
-import { THEME_CHOICES } from '~/constants'
-
-const cookieOptions = {
-  httpOnly: true,
-  path: '/',
-  secure: env.NODE_ENV === 'production',
-}
+import { DEFAULT_COOKIE_OPTIONS, THEME_CHOICES } from '~/constants'
 
 export const server = {
   selectTheme: defineAction({
     input: z.enum(THEME_CHOICES),
     handler: (input, ctx) => {
       if (input === 'system') {
-        ctx.cookies.delete('theme', cookieOptions)
+        ctx.cookies.delete('theme', DEFAULT_COOKIE_OPTIONS)
       } else {
-        ctx.cookies.set('theme', input, cookieOptions)
+        ctx.cookies.set('theme', input, DEFAULT_COOKIE_OPTIONS)
       }
     },
   }),
--- apps/web-client/src/constants.ts
+++ apps/web-client/src/constants.ts
@@ -1,6 +1,18 @@
+import env from '@pkgs/env'
+import type { AstroCookieSetOptions } from 'astro'
+import { addMonths } from 'date-fns'
+
 export const COLOR_SCHEME_HEADER = 'Sec-CH-Prefers-Color-Scheme' as const
 export const THEMES = ['light', 'dark'] as const
 export const THEME_CHOICES = [...THEMES, 'system'] as const
 
 export type Theme = (typeof THEMES)[number]
 export type ThemeChoice = (typeof THEME_CHOICES)[number]
+
+export const DEFAULT_COOKIE_OPTIONS: AstroCookieSetOptions = {
+  httpOnly: true,
+  path: '/',
+  secure: env.NODE_ENV === 'production',
+  sameSite: env.NODE_ENV === 'production' ? 'strict' : 'lax',
+  expires: addMonths(new Date(), 6),
+}
